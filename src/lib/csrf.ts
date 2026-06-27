59bff3db6451f1e0c71dea7cbfa55a027d513213
oliviergenevest <olivier.genevest@gmail.com>
2026-06-27 07:59:31 +0200

    netlify adpater

--- src/lib/csrf.ts
+++ src/lib/csrf.ts
@@ -1,7 +1,8 @@
 export function checkOrigin(request: Request): boolean {
   const origin = request.headers.get('origin')
   const siteUrl = import.meta.env.SITE_URL
-
+ console.log('Origin reçu:', origin)
+  console.log('SITE_URL:', siteUrl)
   // En développement on laisse passer
   if (import.meta.env.DEV) return true
 
